907ba1d89bbf85196f9cb567dc55341a19767290
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-07-23 16:21:55 +0200

    app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main 
+      className="flex flex-wrap justify-center items-center flex-grow
+      bg-gray-100 px-10 md:flex-nowrap">
 
+        {/* <Creation/>
+        <Visualization/> */}
+      </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+  return (
+    <header
+    className="p-4 border-b-2 border-purple-400">
+        <h1 
+        className="text-xl text-center uppercase font-semibold">
+            Boxy Generator
+        </h1>
+    </header>
+  )
+}
