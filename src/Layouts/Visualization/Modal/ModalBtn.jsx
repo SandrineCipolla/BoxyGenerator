b5b8ed8f5233bb296a281798271db6fec02dfc54
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-07-25 14:16:09 +0200

    modal content creation

--- src/Layouts/Visualization/Modal/ModalBtn.jsx
+++ src/Layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import ModalResult from "./ModalResult"
 
 export default function ModalBtn() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className="relative z-0 mx-auto mt-2 py-1 px-3 cursor-pointer text-sm rounded
-     bg-purple-600 text-white hover:bg-purple-700">
+    <>
+      <button
+      onClick={() =>setShowModal(!showModal)}
+        className="relative z-0 mx-auto mt-2 py-1 px-3 cursor-pointer text-sm rounded
+      bg-purple-600 text-white hover:bg-purple-700"
+      >
         Get the code
-    </div>
-  )
-} 
+      </button>
+      {showModal && 
+        createPortal(
+            <ModalResult closeModal={() => setShowModal(!showModal)}/>,
+            document.body
+            )}
+    </>
+  );
+}
--- src/Layouts/Visualization/Modal/modalResult.jsx
+++ src/Layouts/Visualization/Modal/modalResult.jsx
@@ -1,5 +1,41 @@
-export default function modalResult() {
+import getBoxShadowValue from "../../../utils/getBoxShadowValue";
+import { useSelector } from "react-redux";
+
+export default function ModalResult({ closeModal }) {
+  const shadowValues = useSelector((state) => state.shadows);
   return (
-    <div>modalResult</div>
-  )
+    <div
+      onClick={closeModal}
+      className="fixed z-10 inset-0 flex items-center justify-center
+    bg-gray-600/75"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="max-w-[400px] rounded p-7 bg-gray-50 mb-[10vh]"
+      >
+        <div className="flex items-end mb-5">
+          <p className="font-semibold mr-5">Here is your code 🎉</p>
+          <button
+            className="ml-auto mr-2 text-sm bg-purple-600 text-white
+                 hover:bg-purple-700 py-1 px-3 rounded"
+          >
+            Copy
+          </button>
+
+          <button
+            onClick={closeModal}
+            className="text-sm bg-pink-600 text-white
+                 hover:bg-pink-700 py-1 px-3 rounded"
+          >
+            Close
+          </button>
+        </div>
+        <p className="rounded bg-gray-100 p-5">
+          <span className="font font-semibold">
+            box-shadow : </span>
+            <span>{getBoxShadowValue(shadowValues)}</span>
+        </p>
+      </div>
+    </div>
+  );
 }
