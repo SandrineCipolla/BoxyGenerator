ffdcd0a9cbcf01c689929965a2248508aac3855b
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-07-25 12:07:00 +0200

    adding state values ​​to the box

--- src/Layouts/Visualization/Modal/ModalBtn.jsx
+++ src/Layouts/Visualization/Modal/ModalBtn.jsx
@@ -1,9 +1,9 @@
 
 export default function ModalBtn() {
   return (
-    <div className="mx-auto mt-2 py-1 px-3 cursor-pointer text-sm rounded
+    <div className="relative z-0 mx-auto mt-2 py-1 px-3 cursor-pointer text-sm rounded
      bg-purple-600 text-white hover:bg-purple-700">
-        Get the <code></code>
+        Get the code
     </div>
   )
 } 
